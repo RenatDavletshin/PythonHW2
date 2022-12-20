46c0f59d91b7a126c11651388b2d4622d338af71
RenatDavletshin <immortal1307@gmail.com>
2022-12-21 00:39:52 +0500

    Task3End

--- Task2.py
+++ Task2.py
@@ -1,8 +1,16 @@
 # Напишите программу, принимает на вход число N и выдает набор из произведений от 1 до N.
 
-N = int(input('Введите число:'))
-x = 1
-for y in range(N):
-    y = y + 1
-    x = y * x
-print(x)
+# N = int(input('Введите число:'))
+# x = 1
+# for y in range(N):
+#     y = y + 1
+#     x = y * x
+# print(x)
+
+N = int(input('Введите число: '))
+num = []
+value = 1
+for i in range(1, N + 1):
+    num.append(value)
+    value *= i + 1
+print(num)
