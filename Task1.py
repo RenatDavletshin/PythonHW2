118ed1fc010b864a967caba02bc234bafa6ef7cb
RenatDavletshin <immortal1307@gmail.com>
2022-12-20 22:25:46 +0500

    Task2End

--- Task1.py
+++ Task1.py
@@ -1,6 +1,13 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-def digits(num):
-    return sum(map(int, num.replace('.','')))
+# def digits(num):
+#     return sum(map(int, num.replace('.','')))
+# num = input('Введите число: ')
+# print(f' Сумма цифр в числе = {digits(num)}')
+
 num = input('Введите число: ')
-print(f' Сумма цифр в числе = {digits(num)}')
+summ = 0
+for count in str(num):
+    if count != '.':
+        summ += int(count)
+print('Сумма цифр в числе', 'равна',summ)
